37421b71b0f6baef7f73fd2f8237b998d407a24c
Richard Chapman <rcchapman85@gmail.com>
2022-06-17 12:17:46 +0100

    react-performance 06 extra credit 3

--- react-performance/src/exercise/06.js
+++ react-performance/src/exercise/06.js
@@ -106,14 +106,18 @@ function Grid() {
 }
 Grid = React.memo(Grid)
 
-function Cell({row, column}) {
-  const state = useAppState()
-  const cell = state.grid[row][column]
+function withStateSlice(Comp, slice) {
+  const MemoComp = React.memo(Comp)
+  function Wrapper(props) {
+    const state = useAppState()
 
-  return <CellImpl cell={cell} row={row} column={column} />
+    return <MemoComp state={slice(state, props)} {...props} />
+  }
+  Wrapper.displayName = `withStateSlice(${Comp.displayName || Comp.name})`
+  return React.memo(Wrapper)
 }
 
-function CellImpl({cell, row, column}) {
+function Cell({state: cell, row, column}) {
   const dispatch = useAppDispatch()
   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
   return (
@@ -129,7 +133,7 @@ function CellImpl({cell, row, column}) {
     </button>
   )
 }
-CellImpl = React.memo(CellImpl)
+Cell = withStateSlice(Cell, (state, {row, column}) => state.grid[row][column])
 
 function DogNameInput() {
   const [state, dispatch] = useDogState()
