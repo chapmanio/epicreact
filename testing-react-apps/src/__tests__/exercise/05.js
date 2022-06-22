68d1e26641d0cff219bbdce8d4571ba4896e602e
Richard Chapman <rcchapman85@gmail.com>
2022-06-22 14:25:39 +0100

    react-testing 05 extra credit 3

--- testing-react-apps/src/__tests__/exercise/05.js
+++ testing-react-apps/src/__tests__/exercise/05.js
@@ -1,50 +1,56 @@
 // mocking HTTP requests
 // http://localhost:3000/login-submission
 
-import * as React from 'react'
-import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import {build, fake} from '@jackfranklin/test-data-bot'
-import {setupServer} from 'msw/node'
+import * as React from "react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { build, fake } from "@jackfranklin/test-data-bot";
+import { setupServer } from "msw/node";
 
-import {handlers} from 'test/server-handlers'
-import Login from '../../components/login-submission'
+import { handlers } from "test/server-handlers";
+import Login from "../../components/login-submission";
 
 const buildLoginForm = build({
   fields: {
-    username: fake(f => f.internet.userName()),
-    password: fake(f => f.internet.password()),
+    username: fake((f) => f.internet.userName()),
+    password: fake((f) => f.internet.password()),
   },
-})
+});
 
-const server = setupServer(...handlers)
+const server = setupServer(...handlers);
 
-beforeAll(() => server.listen())
-afterAll(() => server.close())
+beforeAll(() => server.listen());
+afterAll(() => server.close());
 
 test(`logging in displays the user's username`, async () => {
-  render(<Login />)
-  const {username, password} = buildLoginForm()
+  render(<Login />);
+  const { username, password } = buildLoginForm();
 
-  await userEvent.type(screen.getByLabelText(/username/i), username)
-  await userEvent.type(screen.getByLabelText(/password/i), password)
+  await userEvent.type(screen.getByLabelText(/username/i), username);
+  await userEvent.type(screen.getByLabelText(/password/i), password);
 
-  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await userEvent.click(screen.getByRole("button", { name: /submit/i }));
 
-  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i));
 
-  expect(screen.getByText(username)).toBeInTheDocument()
-})
+  expect(screen.getByText(username)).toBeInTheDocument();
+});
 
-test('omitting the password results in an error', async () => {
-  render(<Login />)
-  const {username,} = buildLoginForm()
+test("omitting the password results in an error", async () => {
+  render(<Login />);
+  const { username } = buildLoginForm();
 
-  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.type(screen.getByLabelText(/username/i), username);
 
-  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await userEvent.click(screen.getByRole("button", { name: /submit/i }));
 
-  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i));
 
-  expect(screen.getByRole('alert')).toHaveTextContent(/password required/i)
-})
+  expect(screen.getByRole("alert").textContent).toMatchInlineSnapshot(
+    `"password required"`
+  );
+});
