b5931c8c1c0765493991b504be972e7540f27079
Richard Chapman <rcchapman85@gmail.com>
2022-06-23 16:15:46 +0100

    react-testing 07 extra credit 2

--- testing-react-apps/src/__tests__/exercise/07.js
+++ testing-react-apps/src/__tests__/exercise/07.js
@@ -6,10 +6,14 @@ import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
-test('renders with the light styles for the light theme', () => {
-  const Wrapper = ({children}) => (<ThemeProvider initialTheme='light'>{children}</ThemeProvider>)
+function renderWithTheme(ui, { theme = 'light', ...options} = {}) {
+  const Wrapper = ({children}) => (<ThemeProvider initialTheme={theme}>{children}</ThemeProvider>)
 
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  return render(ui, {wrapper: Wrapper, ...options})
+}
+
+test('renders with the light styles for the light theme', () => {
+  renderWithTheme(<EasyButton>Easy</EasyButton>, { theme: 'light' })
 
   const button = screen.getByRole('button', {name: /easy/i})
 
@@ -20,9 +24,7 @@ test('renders with the light styles for the light theme', () => {
 })
 
 test('renders with the dark styles for the dark theme', () => {
-  const Wrapper = ({children}) => (<ThemeProvider initialTheme='dark'>{children}</ThemeProvider>)
-
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  renderWithTheme(<EasyButton>Easy</EasyButton>, { theme: 'dark' })
 
   const button = screen.getByRole('button', {name: /easy/i})
 
